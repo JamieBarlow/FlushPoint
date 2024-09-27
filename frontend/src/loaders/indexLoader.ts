e55bd6084fe0e6cd0368348ae987d0b18609b15d
Jamie Barlow <jamiebarlow@gmail.com>
2024-09-27 13:52:36 +0100

    Add validation for indexLoader

--- frontend/src/loaders/indexLoader.ts
+++ frontend/src/loaders/indexLoader.ts
@@ -1,8 +1,20 @@
 import { defer } from "react-router-dom";
 
 export default async function indexLoader() {
-  const bathrooms = fetch("http://localhost:8000/bathrooms").then((res) =>
-    res.json()
-  );
-  return defer({ bathrooms });
+  try {
+    const res = await fetch("http://localhost:8000/bathrooms");
+    if (!res.ok) {
+      throw new Error(
+        `Error fetching bathrooms: ${res.status} ${res.statusText}`
+      );
+    }
+    const bathrooms = await res.json();
+    return defer({ bathrooms });
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(`Network error: ${error.message}`);
+    } else {
+      throw new Error("An unknown error occured.");
+    }
+  }
 }
